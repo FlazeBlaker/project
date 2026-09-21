ae54c76f6020e990f6e5d821a3bef52b12a2e005
FlazeBlaker <jaypawar1205@gmail.com>
2026-09-21 17:00:27 +0530

    Add login function

--- code.ts
+++ code.ts
@@ -1,1 +1,10 @@
 console.log("Hello from the app");
+
+
+function login(username) {
+    console.log(`Logging in ${username}`);
+}
+
+function logout(username) {
+    console.log(`Logging out ${username}`);
+}
